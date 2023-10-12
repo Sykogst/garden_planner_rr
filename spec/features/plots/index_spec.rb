67ef7eab06f5ca4c5f93f62f52b95430503dd19f
Sam Tran <samuelttran@gmail.com>
2023-10-11 21:45:38 -0600

    Correct spec and index.html, /plots was only supposed to display the plot name and no attributes

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -5,17 +5,20 @@ RSpec.describe 'Plots index', type: :feature do
     @plot1 = Plot.create!(name: 'Lawn', arable: true, area_sqft: 100.0)
     @plot2 = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
   end
-  # User Story 1, Parent Index 
 
-  # For each parent table
-  # As a visitor
-  # When I visit '/parents'
-  # Then I see the name of each parent record in the system
-  describe 'When a user visits /parents' do
-    it 'They see the name of each parent record in the system' do
+  # User Story 1, Parent Index 
+  describe 'When a user visits /plots' do
+    it 'They see the name of each plots record in the system' do
       visit '/plots'
 save_and_open_page
       expect(page).to have_content("All Plots of Land")
+      expect(page).to have_content(@plot1.name)
+      expect(page).to have_content(@plot2.name)
+    end
+  end
+
+  describe 'When a user visits /plots/:id' do
+    xit 'They see the plot with that id includings its attributes' do
       expect(page).to have_content(@plot1.name)
       expect(page).to have_content("Contains : plants")
       expect(page).to have_content("Total Area : #{@plot1.area_sqft} sq ft")
