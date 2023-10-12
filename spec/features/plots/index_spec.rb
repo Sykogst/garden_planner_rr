2bb2df4b077e2c1ec491997b6ace08834a74c668
Sam Tran <samuelttran@gmail.com>
2023-10-12 15:08:35 -0600

    Move test for plot organisms to its own spec file and directory

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Plots index', type: :feature do
   before(:each) do
     @plot1 = Plot.create!(name: 'Lawn', arable: true, area_sqft: 100.0)
-    @org1 = @plot1.organisms.create!(name: 'Clover', plant: true, max_size_sqft: 50.0)
-
     @plot2 = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
-    @org2 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-    @org3 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-    @org4 = @plot2.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0)
   end
 
   # User Story 1, Plots Index 
@@ -19,29 +14,6 @@ RSpec.describe 'Plots index', type: :feature do
       expect(page).to have_content("All Plots of Land")
       expect(page).to have_content(@plot1.name)
       expect(page).to have_content(@plot2.name)
-    end
-  end
-
-  # User Story 5, Parent Children Index 
-  describe 'When a user visits /plots/:plot_id/organisms' do
-    it 'Then they see each organism and its attributes that is associated with that plot id' do
-      visit "/plots/#{@plot1.id}/organisms"
-
-      expect(page).to have_content(@plot1.name)
-      expect(page).to have_content("#{@org1.name} is a plant")
-      expect(page).to have_content("Space Taken : #{@org1.max_size_sqft} sq ft")
-    end
-
-    it 'Then they see each organism and its attributes that is associated with that plot id' do
-      visit "/plots/#{@plot2.id}/organisms"
-
-      expect(page).to have_content(@plot2.name)
-      expect(page).to have_content("#{@org2.name} is an animal")
-      expect(page).to have_content("Space Taken : #{@org2.max_size_sqft} sq ft")
-      expect(page).to have_content("#{@org3.name} is an animal")
-      expect(page).to have_content("Space Taken : #{@org3.max_size_sqft} sq ft")
-      expect(page).to have_content("#{@org4.name} is an animal")
-      expect(page).to have_content("Space Taken : #{@org4.max_size_sqft} sq ft")
     end
   end
 end
