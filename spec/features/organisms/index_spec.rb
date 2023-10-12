c63e5808257ae2f119b09dd999ab0ef117da5bca
Sam Tran <samuelttran@gmail.com>
2023-10-11 22:28:51 -0600

    Add type: :model to model tests

---
+++ spec/features/organisms/index_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Organisms show', type: :feature do
+  before(:each) do
+    @plot1 = Plot.create!(name: 'Lawn', arable: true, area_sqft: 100.0)
+    @plot2 = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
+  end
+end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Plot do
+RSpec.describe Plot, type: :model do
   it { should have_many :organisms }
 end
