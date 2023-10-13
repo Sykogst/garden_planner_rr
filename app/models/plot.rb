9554ccca86c501a176e77aef8c47480246cbe68a
Sam Tran <samuelttran@gmail.com>
2023-10-13 16:49:04 -0600

    Change a comment

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,6 +1,6 @@
 class Plot < ApplicationRecord
   has_many :organisms
-  default_scope { order(created_at: :asc) } # Not sure if the sort should go here or in controller
+  default_scope { order(created_at: :asc) } # Not sure if the order should be a method, or in a controller
 
   def organisms_count
     self.organisms.count
