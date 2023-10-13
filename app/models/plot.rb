317aba78ceb6b13e09656dd3c6f847acf7ad0130
Sam Tran <samuelttran@gmail.com>
2023-10-12 18:01:20 -0600

    Rewrite test for organism count on plot show page, add method to Plot model

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,4 +1,8 @@
 class Plot < ApplicationRecord
   has_many :organisms
-  default_scope { order(created_at: :asc) }
+  default_scope { order(created_at: :asc) } # Not sure if the sort should go here or in controller
+
+  def organism_count
+    self.organisms.count
+  end
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Plots index', type: :feature do
   before(:each) do
     old_date = '2022-10-12 [22:22:22]'
-    oldest_date = '2021-10-12 [22:22:22]'
+    oldest_date = '2021-10-12'
     @plot1 = Plot.create!(name: 'Lawn', arable: true, area_sqft: 100.0)
     @plot2 = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0, created_at: oldest_date)
     @plot3 = Plot.create!(name: 'Flower Bed', arable: true, area_sqft: 10.0, created_at: old_date)
@@ -28,7 +28,7 @@ RSpec.describe 'Plots index', type: :feature do
       expect(page).to have_content(@plot1.created_at)
       expect(page).to have_content(@plot2.created_at)
       expect(page).to have_content(@plot3.created_at)
-save_and_open_page
+
       expect(@plot2.name).to appear_before(@plot1.name)
       expect(@plot3.name).to appear_before(@plot1.name)
       expect(@plot2.name).to appear_before(@plot3.name)
