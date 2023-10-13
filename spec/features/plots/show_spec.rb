84ee50b10f9e2e58a869ad2bb0645fdbc1b43a6b
Sam Tran <samuelttran@gmail.com>
2023-10-13 07:01:38 -0600

    Write tests for link on /plots/:id to /plots/:plot_id/organisms

--- spec/features/plots/show_spec.rb
+++ spec/features/plots/show_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Plots show', type: :feature do
   before(:each) do
     @plot1 = Plot.create!(name: 'Lawn', arable: true, area_sqft: 100.0)
+    @org1 = @plot1.organisms.create!(name: 'Clover', plant: true, max_size_sqft: 50.0)
+
     @plot2 = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
+    @org2 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
+    @org3 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
+    @org4 = @plot2.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0)
   end
 
   # User Story 2, Plots Show 
@@ -25,15 +30,8 @@ RSpec.describe 'Plots show', type: :feature do
     end
   end
 
-  # User Story 7, Parent Child Count
+  # User Story 7, Plot Organisms Count
   describe 'When a user visits /plots/:id' do
-    before(:each) do
-      @org1 = @plot1.organisms.create!(name: 'Clover', plant: true, max_size_sqft: 50.0)
-      @org2 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-      @org3 = @plot2.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-      @org4 = @plot2.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0)
-    end
-
     it 'They see the plot with that id including count of organisms' do
       visit "plots/#{@plot1.id}"
 
@@ -51,15 +49,10 @@ RSpec.describe 'Plots show', type: :feature do
 
   # User Story 8, Organisms Index Link
   describe 'When a user visits /plots/:id' do
-    it 'They see a link that takes them back to /organisms' do
+    it 'They see a link that takes them back to /organisms, at top of page' do
       visit "plots/#{@plot1.id}"
 
       expect(page).to have_link('All Organisms', :href=>'/organisms')
-    end
-
-    it 'They see a link bar at top of page' do
-      visit "plots/#{@plot1.id}"
-
       expect(page.find('.topBar')).to appear_before(@plot1.name)
     end
 
@@ -78,17 +71,12 @@ RSpec.describe 'Plots show', type: :feature do
     end
   end
 
-  # User Story 9, Parent Index Link
+  # User Story 9, Plots Index Link
   describe 'When a user visits /plots/:id' do
-    it 'They see a link that takes them back to /plots' do
+    it 'They see a link that takes them back to /plots, link bar at top of page' do
       visit "/plots/#{@plot1.id}"
 
       expect(page).to have_link('All Plots', :href=>'/plots')
-    end
-
-    it 'They see a link bar at top of page' do
-      visit "/plots/#{@plot1.id}"
-
       expect(page.find('.topBar')).to appear_before(@plot1.name)
     end
 
@@ -104,6 +92,29 @@ RSpec.describe 'Plots show', type: :feature do
       click_on('All Plots')
 
       expect(current_path).to eq('/plots')
+    end
+  end
+
+  # User Story 10, Plots Organisms Index Link
+  describe 'When a user visits a plots/:id' do
+    it 'They see a link that takes them to /plots/:plot_id/organisms' do
+      visit "/plots/#{@plot1.id}"
+
+      expect(page).to have_link('See Organisms', :href=>"/plots/#{@plot1.id}/organisms")
+    end
+
+    xit 'Takes user to /plots/:plot_id/organisms after clicking on link' do
+      visit "/plots/#{@plot1.id}"
+      click_on('See Organisms')
+
+      expect(current_path).to eq("/plots/#{@plot1.id}/organisms")
+    end
+
+    xit 'Works on another /plots/:plot_id/organisms page with another id' do
+      visit "/plots/#{@plot2.id}"
+      click_on('See Organisms')
+
+      expect(current_path).to eq("/plots/#{@plot2.id}/organisms")
     end
   end
 end
