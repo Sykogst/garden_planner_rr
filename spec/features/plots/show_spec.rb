87f34e2b7cad1dffb5bdff7cd1a87ea2f87ab18d
Sam Tran <samuelttran@gmail.com>
2023-10-13 20:43:14 -0600

    Write test for update plots record and add link on plot/:plot_id show page

--- spec/features/plots/show_spec.rb
+++ spec/features/plots/show_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Plots show', type: :feature do
   end
 
   # User Story 7, Plot Organisms Count
-  describe 'When a user visits /plots/:id' do
+  describe 'When a user visits /plots/:id, it includes count of organisms' do
     it 'They see the plot with that id including count of organisms' do
       visit "plots/#{@plot1.id}"
 
@@ -48,7 +48,7 @@ RSpec.describe 'Plots show', type: :feature do
   end
 
   # User Story 8, Organisms Index Link
-  describe 'When a user visits /plots/:id' do
+  describe 'When a user visits /plots/:id there is a link to go back to organisms' do
     it 'They see a link that takes them back to /organisms, at top of page' do
       visit "plots/#{@plot1.id}"
 
@@ -72,7 +72,7 @@ RSpec.describe 'Plots show', type: :feature do
   end
 
   # User Story 9, Plots Index Link
-  describe 'When a user visits /plots/:id' do
+  describe 'When a user visits /plots/:id there is a link to go back to all plots' do
     it 'They see a link that takes them back to /plots, link bar at top of page' do
       visit "/plots/#{@plot1.id}"
 
@@ -96,7 +96,7 @@ RSpec.describe 'Plots show', type: :feature do
   end
 
   # User Story 10, Plots Organisms Index Link
-  describe 'When a user visits a plots/:id' do
+  describe 'When a user visits a plots/:id there is a link to all organisms on this plot' do
     it 'They see a link that takes them to /plots/:plot_id/organisms' do
       visit "/plots/#{@plot1.id}"
       
@@ -117,4 +117,42 @@ RSpec.describe 'Plots show', type: :feature do
       expect(current_path).to eq("/plots/#{@plot2.id}/organisms")
     end
   end
+
+  # User Story 12, Plot Update 
+  describe 'When a user visits /plots/id there is a link to update this record' do
+    it 'They see a link /plots/new to update current plot record, New Plot' do
+      visit "/plots/#{@plot1.id}"
+# save_and_open_page
+      expect(page).to have_link('Update Plot', :href=>"/plots/#{@plot1.id}/edit")
+    end
+
+    xit 'Takes user to /plots/:plot_id/edit after clicking on link' do
+      visit "/plots/#{@plot1.id}"
+      click_on('Update Plot')
+
+      expect(page).to have_content("Update #{@plot1.name}")
+      expect(current_path).to eq("/plots/#{@plot1.id}/edit")
+    end
+
+    xit 'User fills in form for plot with proper attributes, hit button Submit Update, then redirects back to /plots with updates' do
+      visit "/plots/#{@plot1.id}/edit"
+
+      fill_in('name', with: 'Pumpkin Patch')
+      click_on('Submit')
+
+      expect(current_path).to eq("/plots/#{@plot1.id}")
+      # Test to see that form did not update this attribute
+      expect(page).to have_content("Total Area : 100.0 sq ft")
+    end
+  end
+# As a visitor
+# When I visit a parent show page
+# Then I see a link to update the parent "Update Parent"
+# When I click the link "Update Parent"
+# Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/parents/:id',
+# the parent's info is updated,
+# and I am redirected to the Parent's Show page where I see the parent's updated info
 end
