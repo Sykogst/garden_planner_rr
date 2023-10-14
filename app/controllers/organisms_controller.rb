505fdd20cd9707caefb98bc9e4b6e1afd6492062
Sam Tran <samuelttran@gmail.com>
2023-10-14 11:14:50 -0600

    Update controllers to incorporate new alive field

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -20,6 +20,6 @@ class OrganismsController < ApplicationController
 
   private
     def organism_params
-      params.permit(:name, :plant, :max_size_sqft)
+      params.permit(:name, :plant, :max_size_sqft, :alive)
     end
 end
