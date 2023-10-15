c831cb34ac8b5b51184d9c3465b75397111f0072
Sam Tran <samuelttran@gmail.com>
2023-10-15 11:12:45 -0600

    Add #set_organism private method as it was repeated a few times

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -1,24 +1,27 @@
 class OrganismsController < ApplicationController
+  before_action :set_organism, only: [:show, :edit, :update]
+
   def index
     @organisms = Organism.alive_true_all
   end
 
   def show
-    @organism = Organism.find(params[:id])
   end
 
   def edit
-    @organism = Organism.find(params[:id])
   end
 
   def update
-    organism = Organism.find(params[:id])
-    organism.update(organism_params.compact_blank)
-    organism.save
-    redirect_to "/organisms/#{organism.id}"
+    @organism.update(organism_params.compact_blank)
+    @organism.save
+    redirect_to "/organisms/#{@organism.id}"
   end
 
   private
+    def set_organism
+      @organism = Organism.find(params[:id])
+    end
+
     def organism_params
       params.permit(:name, :plant, :max_size_sqft, :alive)
     end
