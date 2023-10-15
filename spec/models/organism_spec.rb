d35db425a8ab9eeea0b49bb35a5db583a6e0b5ba
Sam Tran <samuelttran@gmail.com>
2023-10-15 09:52:27 -0600

    Write model test for sort by alive true only #only_alive

--- spec/models/organism_spec.rb
+++ spec/models/organism_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Organism, type: :model do
   it { should belong_to :plot }
+
+  describe '#only_alive' do
+    it 'returns list of true only' do
+      plot = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
+      org1 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0, alive: true)
+      org2 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0, alive: false)
+      org3 = plot.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0, alive: true)
+
+      plot2 = Plot.create!(name: 'Lawn', arable: false, area_sqft: 50.0)
+      org4 = plot.organisms.create!(name: 'Clover', plant: true, max_size_sqft: 5.0, alive: true)
+
+      expect(Organism.only_alive.count).to eq(3)
+    end
+  end
 end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Plot, type: :model do
   describe '#organisms_count' do
     it 'returns count of organisms on a particular plot' do
       plot = Plot.create!(name: 'Coop', arable: false, area_sqft: 50.0)
-      org1 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-      org2 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0)
-      org3 = plot.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0)
+      org1 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0, alive: true)
+      org2 = plot.organisms.create!(name: 'Chicken', plant: false, max_size_sqft: 5.0, alive: false)
+      org3 = plot.organisms.create!(name: 'Rooster', plant: false, max_size_sqft: 5.0, alive: true)
 
       expect(plot.organisms_count).to eq(3)
     end
